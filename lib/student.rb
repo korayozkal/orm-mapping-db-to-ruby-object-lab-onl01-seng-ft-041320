8d4b192ea201942df35a7130c0fa1e89487b1215
Koray Ozkal <koryozkal@gmail.com>
2020-05-24 04:47:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,16 @@
 class Student
   attr_accessor :id, :name, :grade
 
+ class Student
+  attr_accessor :id, :name, :grade	  attr_accessor :id, :name, :grade
+
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
-  end
+    # create a new Student object given a row from the database	    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end	  end
 
   def self.all
     # retrieve all the rows from the "Students" database
